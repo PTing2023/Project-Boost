54394b1721d17303ba8df0176c849cbf771f3d27
PTing2023 <robloxweirdo625@gmail.com>
2023-06-15 17:56:45 -0400

    lots o changes, finished

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -6,27 +6,48 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 100;
     [SerializeField] float rotationThrust = 1f;
+    [SerializeField] AudioClip mainEngine;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem turnLeftThrusterParticles;
+    [SerializeField] ParticleSystem turnRightThrusterParticles;
+
     Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ProcessThrust();
         ProcessRotation();
     }
 
-    void ProcessInput()
+    void ProcessThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+            if (!mainEngineParticles.isPlaying) {
+                mainEngineParticles.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+   
     }
 
     void ProcessRotation()
@@ -34,10 +55,23 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             ApplyRotation(rotationThrust);
+
+            if (!turnLeftThrusterParticles.isPlaying)
+            {
+                turnLeftThrusterParticles.Play();
+            }
         }
         else if (Input.GetKey(KeyCode.D))
         {
             ApplyRotation(-rotationThrust);
+
+            if (!turnRightThrusterParticles.isPlaying)
+            {
+                turnRightThrusterParticles.Play();
+            }
+        } else {
+            turnLeftThrusterParticles.Stop();
+            turnRightThrusterParticles.Stop();
         }
     }
 
